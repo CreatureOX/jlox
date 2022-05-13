b289486615b02756903934ee85dee05942a55354
CreatureOX <creatureox0726@gmail.com>
2022-05-13 21:09:55 +0800

    control flow

--- src/main/java/com/github/creatureox/lox/Expr.java
+++ src/main/java/com/github/creatureox/lox/Expr.java
@@ -6,6 +6,7 @@ public abstract class Expr {
         R visitBinaryExpr(Binary expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
+        R visitLogicalExpr(Logical expr);
         R visitUnaryExpr(Unary expr);
         R visitVariableExpr(Variable expr);
     }
@@ -97,6 +98,23 @@ public abstract class Expr {
         }
 
         final Token name;
+    }
+
+    static class Logical extends Expr {
+        Logical(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLogicalExpr(this);
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
     }
 
 }
